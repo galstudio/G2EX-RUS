2ef169fa94eb171f9c245c88a5fca3a81b5d0cee
galstudio <galstudio@gmail.com>
2019-01-24 17:31:14 +0300

    Initial Commit

--- core/install_update/views/upgrade/completed.php
+++ core/install_update/views/upgrade/completed.php
@@ -8,14 +8,14 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 $settings = Yii::$app->params['settings'];
-$this->title = Html::encode($settings['site_name']) .' › 升级完成';
+$this->title = Html::encode($settings['site_name']) .' › Обновление завершено';
 ?>
 </div>
 <div id="Main" style="margin: 0px 20px 0px 20px;">
 <div class="sep20"></div>
 <div class="box">
-<div class="header"><?php echo Html::a(Html::encode($settings['site_name']) , ['/']), '<span class="chevron">&nbsp;›&nbsp;</span>升级完成';?></div>
-<div class="problem">升级完成！ </div>
-<div class="cell"><?php echo Html::a('登录管理后台更新论坛配置', ['/admin/setting/clear-cache'],['class' => 'super normal button']); ?></div>
+<div class="header"><?php echo Html::a(Html::encode($settings['site_name']) , ['/']), '<span class="chevron">&nbsp;›&nbsp;</span>Обновление завершено';?></div>
+<div class="problem">Обновление завершено! </div>
+<div class="cell"><?php echo Html::a('Вход в админпанель', ['/admin/setting/clear-cache'],['class' => 'super normal button']); ?></div>
 </div>
 </div>
--- core/install_update/views/upgrade/index.php
+++ core/install_update/views/upgrade/index.php
@@ -8,14 +8,14 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 $settings = Yii::$app->params['settings'];
-$this->title = Html::encode($settings['site_name']) .' › 系统升级';
+$this->title = Html::encode($settings['site_name']) .' › Обновление системы';
 ?>
 </div>
 <div id="Main" style="margin: 0px 20px 0px 20px;">
 <div class="sep20"></div>
 <div class="box">
-<div class="header"><?php echo Html::a(Html::encode($settings['site_name']) , ['/']), '<span class="chevron">&nbsp;›&nbsp;</span>系统升级';?></div>
-<div class="problem">请先备份网站程序，并备份数据库数据。本站不对升级过程中造成的程序或数据损失等负责。 </div>
-<div class="cell"><?php echo Html::a('已备份，开始升级', ['v115to120'],['class' => 'super normal button']); ?></div>
+<div class="header"><?php echo Html::a(Html::encode($settings['site_name']) , ['/']), '<span class="chevron">&nbsp;›&nbsp;</span>Обновление системы';?></div>
+<div class="problem">Пожалуйста, сначала сделайте резервную копию сайта и базы данных. Мы не несем ответственности за потерю данных, вызванную процессом обновления.</div>
+<div class="cell"><?php echo Html::a('Резервное копирование, обновление', ['v115to120'],['class' => 'super normal button']); ?></div>
 </div>
 </div>
